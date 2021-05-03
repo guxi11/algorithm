a0bd5ff23b2e8739dccb8ee052e137de202830fc
Guxi11 <guxi11@isanbel.com>
2021-05-03 21:50:37 +0800

    1. 两数之和

--- leetcode/3. 无重复字符的最长子串.js
+++ leetcode/3. 无重复字符的最长子串.js
@@ -1,19 +1,19 @@
-// tags: array, string, sliding window
+// array, string, sliding window
 /**
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
-  let left = right = 0, window = new Set(s[0] || null);
-  let max = window.size;
-  while (++right < s.length) {
-      let cr = s[right];
-      while (window.has(cr)) {
-          window.delete(s[left++]);
-      }
-      window.add(cr);
+var lengthOfLongestSubstring = function(s) {
+    let left = right = 0, window = new Set(s[0] || null);
+    let max = window.size;
+    while (++right < s.length) {
+        let cr = s[right];
+        while (window.has(cr)) {
+            window.delete(s[left++]);
+        }
+        window.add(cr);
 
-      max = Math.max(window.size, max);
-  }
-  return max;
+        max = Math.max(window.size, max);
+    }
+    return max;
 };
